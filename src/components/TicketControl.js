7bd5a1dc0a20961809bece7c341c26231e5b6d6e
ian-bravo <bravo.ian@gmail.com>
2023-12-05 14:38:33 -0800

    add Read fx - react

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect ,useState } from "react";
 import NewTicketForm from "./NewTicketForm";
 import TicketList from "./TicketList";
 import EditTicketForm from "./EditTicketForm";
 import TicketDetail from "./TicketDetail";
 import db from './../firebase.js';
+import { collection, addDoc, onSnapshot, deleteDoc } from "firebase/firestore";
+
 
 function TicketControl() {
   // constructor(props) {
@@ -19,6 +21,30 @@ function TicketControl() {
   const [mainTicketList, setMainTicketList] = useState([]);
   const [selectedTicket, setSelectedTicket] = useState(null);
   const [editing, setEditing] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => { 
+    const unSubscribe = onSnapshot(
+      collection(db, "tickets"), 
+      (collectionSnapshot) => {
+        const tickets = [];
+        collectionSnapshot.forEach((doc) => {
+            tickets.push({
+              names: doc.data().names, 
+              location: doc.data().location, 
+              issue: doc.data().issue, 
+              id: doc.id
+            });
+        });
+        setMainTicketList(tickets);
+      }, 
+      (error) => {
+        setError(error.message);
+      }
+    );
+
+    return () => unSubscribe();
+  }, []);
 
   const handleClick = () => {
     if (selectedTicket != null) {
@@ -67,6 +93,7 @@ function TicketControl() {
     await addDoc(collectionRef, newTicketData);
     setFormVisibleOnPage(false);
   }
+
   //same as above
   // const handleAddingNewTicketToList = async (newTicketData) => {
   //   await addDoc(collection(db, "tickets"), newTicketData);
@@ -90,7 +117,9 @@ function TicketControl() {
 
   let currentlyVisibleState = null;
   let buttonText = null;
-  if (editing) {
+  if (error){
+    currentlyVisibleState = <p>There was an error: {error}</p>
+  } else if (editing) {
     currentlyVisibleState = (
       <EditTicketForm
         ticket={selectedTicket}
@@ -123,7 +152,7 @@ function TicketControl() {
   return (
     <React.Fragment>
       {currentlyVisibleState}
-      <button onClick={handleClick}>{buttonText}</button>
+      {error ? null : <button onClick={handleClick}>{buttonText}</button>}
     </React.Fragment>
   );
 }
